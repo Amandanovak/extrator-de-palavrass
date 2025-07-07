30f87c000afe82145e14ad356dc6e62873b1f5b5
Amandanovak <amanda.nascimento.novak@escola.pr.gov.br>
2025-07-07 13:47:13 +0000

    .

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-botaoMostraPalavras.addEventListener('click',mostrapalavraschave);
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-    function mostrapalavraschave(){
-        const texto = document.querySelector('#entrada-de-texto').value;
-        const campoResultado = document.querySelector('#resultado-palavrachave');
-        const palavras = texto.split(" ");
+function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
 
-        campoResultado.textContent = palavras.join(", ");
-    }
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  return palavras;
+}
